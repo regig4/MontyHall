fc6782a12181c498ac32ad21daeccd46ea608747
regig4 <regig4@gmail.com>
2024-08-05 19:03:05 +0200

    add C implementation

---
+++ c/monty.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+#include <stdlib.h>
+
+uint32_t experiment(uint32_t n)
+{
+    uint32_t winCount = 0;
+
+    for (size_t i; i < n; i++)
+    {
+        uint32_t prize = rand() % 3;
+        uint32_t choice = rand() % 3;
+
+        if (choice == prize)
+        {
+            winCount++;
+        }
+    }
+    return winCount;
+}
+
+uint32_t experiment2(uint32_t n)
+{
+    uint32_t winCount = 0;
+
+    for (size_t i = 0; i < n; i++)
+    {
+        uint32_t prize = rand() % 3;
+        uint32_t choice = rand() % 3;
+
+        short *possible;
+        int possibleCount;
+
+        if (choice == prize)
+        {
+            possibleCount = 3;
+            possible = malloc(sizeof(short) * possibleCount);
+            for (int j = 0; j < possibleCount; j++)
+            {
+                possible[j] = j;
+            }
+        }
+        else
+        {
+            possibleCount = 2;
+            possible = malloc(sizeof(short) * possibleCount);
+            possible[0] = choice;
+            possible[1] = prize;
+        }
+
+        choice = possible[rand() % possibleCount];
+
+        if (choice == prize)
+        {
+            winCount++;
+        }
+    }
+
+    return winCount;
+}
+
+int main(void)
+{
+    time_t tt;
+    int seed = time(&tt);
+    srand(seed);
+
+    const uint32_t count = 1000000;
+    uint32_t winCount = experiment(count);
+    printf("experiment #1 probability: %d\n", winCount * 100 / count);
+    uint32_t winCount2 = experiment2(count);
+    printf("experiment #2 probability: %d", winCount2 * 100 / count);
+
+    return 0;
+}
