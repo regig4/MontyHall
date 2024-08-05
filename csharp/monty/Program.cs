9c2ddce7eec4fb1f7dab17fa09c5d539b7001ee1
regig4 <regig4@gmail.com>
2024-08-05 21:40:42 +0200

    add C# implementation

---
+++ csharp/monty/Program.cs
@@ -0,0 +1,32 @@
+﻿Random rand = new();
+
+const int N = 1_000_000;
+
+var winCount = Enumerable.Range(0, N)
+                         .Select(x => new
+                         {
+                             choice = rand.Next(0, 3),
+                             prize = rand.Next(0, 3)
+                         })
+                         .Where(x => x.choice == x.prize)
+                         .Count();
+
+var winCount2 = Enumerable.Range(0, N)
+                          .Select(x => new
+                          {
+                              choice = rand.Next(0, 3),
+                              prize = rand.Next(0, 3)
+                          })
+                          .Select(x => new
+                          {
+                              prize = x.prize,
+                              choice = x.prize == x.choice 
+                                                    ? rand.GetItems([0, 1, 2], 1)[0] 
+                                                    : rand.GetItems([x.choice, x.prize], 1)[0]
+                          })
+                          .Where(x => x.choice == x.prize)
+                          .Count();
+
+
+Console.WriteLine($"experiment #1 probability: {winCount * 100 / N}");
+Console.WriteLine($"experiment #2 probability: {winCount2 * 100 / N}");
